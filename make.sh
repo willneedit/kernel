deb59a7108ec73c39e803e7a3216fc667d663a81
zyw <zyw@rock-chips.com>
2014-08-07 18:35:53 +0800

    update make.sh 1. use "make.sh"to  make veyron board; 2. use "make.sh evb" to make sdk_rk808 board

--- make.sh
+++ make.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-./chromeos/scripts/prepareconfig chromiumos-rockchip
+
+DTB=rk3288-veyron.dtb
+if [ "$1" = "evb" ];then
+	DTB=rk3288-evb-rk808.dtb
+fi
 export ARCH=arm
 export CROSS_COMPILE=armv7a-cros-linux-gnueabi-
-make oldnoconfig
-make zImage -j4
-make rk3288-veyron-rk808.dtb
+./chromeos/scripts/prepareconfig chromiumos-rockchip && \
+make oldnoconfig && \
+make zImage -j4 && \
+make $DTB && \
+cp arch/arm/boot/dts/$DTB rk3288.dtb && \
+./build_gpt_kernel.sh
 #cat arch/arm/boot/zImage arch/arm/boot/dts/rk3288-tmp.dtb > arch/arm/boot/kernel
 #./mkkrnlimg arch/arm/boot/kernel kernel.img
-./build_gpt_kernel.sh
-
-
