e4a3e22d6e9a60cbd42649eb52ef13580c97699c
zyw <zyw@rock-chips.com>
2014-08-07 17:12:51 +0800

    keyboard

---
+++ build_gpt_kernel.sh
@@ -0,0 +1,2 @@
+mkimage -D "-I dts -O dtb -p 1024" -f its_script kernel.tmp
+~/trunk/src/scripts/build_kernel_image.sh --board veyron --enable_serial ttyS2 --noenable_rootfs_verification false --use_dev_keys true --arch arm --keys_dir ~/trunk/src/platform/vboot_reference/tests/devkeys/ --vmlinuz kernel.tmp --to kernel.img
