00329b5878bb7e80fd709b57bea10f8b52a7faf9
zyw <zyw@rock-chips.com>
2014-08-07 11:57:04 +0800

    Veyron: rename dts file from evb to veyron
    Signed-off-by: zyw <zyw@rock-chips.com>

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ export ARCH=arm
 export CROSS_COMPILE=armv7a-cros-linux-gnueabi-
 make oldnoconfig
 make zImage -j4
-make rk3288-evb-rk808.dtb
+make rk3288-veyron-rk808.dtb
 #cat arch/arm/boot/zImage arch/arm/boot/dts/rk3288-tmp.dtb > arch/arm/boot/kernel
 #./mkkrnlimg arch/arm/boot/kernel kernel.img
 ./build_gpt_kernel.sh
