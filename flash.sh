ff20ca960a589445f88b96a75e7555149794a2b0
Kever Yang <kever.yang@rock-chips.com>
2014-08-08 15:04:35 +0800

    add support for image upgrade - add loader.bin for upgrade, so we don't need to get from ftp - add sudo in flash.sh - auto reset device after kernel upgrade
    Change-Id: I6ed58a7446b2414e76febe60ac0c6a498c241892

--- flash.sh
+++ flash.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./upgrade_tool db loader.bin
-./upgrade_tool wl 20480 kernel.img
+sudo ./upgrade_tool db loader.bin
+sudo ./upgrade_tool wl 20480 kernel.img
+sudo ./upgrade_tool rd
 ls >/dev/null
