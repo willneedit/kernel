2a14a56b83da6786df1c61e57c33e7a9f3484271
ZhengShunQian <zhengsq@rock-chips.com>
2014-08-09 14:33:55 +0800

    update make.sh, to compat modules generated by ebuild.
    Change-Id: Ic9f5162ad42f020464232e9a4c9c8bef11a2cb3c

--- make.sh
+++ make.sh
@@ -7,8 +7,9 @@ fi
 export ARCH=arm
 export CROSS_COMPILE=armv7a-cros-linux-gnueabi-
 ./chromeos/scripts/prepareconfig chromiumos-rockchip && \
+echo "CONFIG_VLAN_8021Q=m\nCONFIG_USB_NET_CDC_MBIM=m\nCONFIG_USB_WDM=m" >> .config && \
 make oldnoconfig && \
-make zImage -j4 && \
+make zImage -j`grep -r processor /proc/cpuinfo |wc -l` && \
 make $DTB && \
 cp arch/arm/boot/dts/$DTB rk3288.dtb && \
 ./build_gpt_kernel.sh
