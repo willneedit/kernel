11f0b41e890e977072f92c8be6c796ecc82c6344
jeffy <jeffy@localhost.localdomain>
2014-08-09 14:30:36 +0800

    update save_config.sh

--- save_config.sh
+++ save_config.sh
@@ -6,3 +6,5 @@ sed -i "s/silentoldconfig/oldnoconfig/" ./chromeos/scripts/kernelconfig
 ./chromeos/scripts/kernelconfig oldconfig
 git checkout ./chromeos/scripts/kernelconfig
 mv chromeos/config/armel/rk3288.config chromeos/config/armel/chromiumos-rockchip.flavour.config
+git show 1b3f7a0f16215acef99743bc595ef5b29dac4f65 | patch -p1
+git diff
