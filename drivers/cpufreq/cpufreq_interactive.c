aaa5a035e7a04ff1e9dad5ed0ca328f3df9e9f1e
Jianqun <jay.xu@rock-chips.com>
2014-08-25 11:53:34 -0700

    CPUFREQ: fix NULL point cause kernel oops
    For interactive, it should to creat "cpufreq" dir for itself.
    Without this, system will call a NULL object, that should cause
    kernel oops.
    
    Change-Id: I5cd8620684c5543c8e92accdf301f46c531e2441

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -984,7 +984,10 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		if (atomic_inc_return(&active_count) > 1)
 			return 0;
 
-		rc = sysfs_create_group(cpufreq_global_kobject,
+		if (!have_governor_per_policy())
+			WARN_ON(cpufreq_get_global_kobject());
+
+		rc = sysfs_create_group(get_governor_parent_kobj(policy),
 				&interactive_attr_group);
 		if (rc)
 			return rc;
