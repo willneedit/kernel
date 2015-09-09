5ff20c64d49e8c19438326f9bf57a23dd51066b1
willneedit <willneedit@nurfuerspam.de>
2015-09-09 06:04:57 +0200

    Adapted to my needs.
    Added rk3188-odysnoon.dts to describe my hardware

--- make.sh
+++ make.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #./chromeos/scripts/prepareconfig chromiumos-rockchip
-export ARCH=arm
-export CROSS_COMPILE=armv7a-cros-linux-gnueabi-
-make oldnoconfig
+#make oldnoconfig
 make zImage -j48
-make rk3288-veyron.dtb
-#cat arch/arm/boot/zImage arch/arm/boot/dts/rk3288-tmp.dtb > arch/arm/boot/kernel
+make rk3188-odysnoon.dtb
+cat arch/arm/boot/zImage arch/arm/boot/dts/rk3188-odysnoon.dtb > Kernel
 #./mkkrnlimg arch/arm/boot/kernel kernel.img
-./build_gpt_kernel.sh
+#./build_gpt_kernel.sh
 
 
